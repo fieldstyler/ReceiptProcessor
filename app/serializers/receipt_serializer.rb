9235ef92065d9b891d42c39a6cfe7c741f061925
fieldstyler <tdfields93@gmail.com>
2025-02-02 12:19:42 -0700

    rubocop autocorrect

--- app/serializers/receipt_serializer.rb
+++ app/serializers/receipt_serializer.rb
@@ -1,9 +1,9 @@
 class ReceiptSerializer
     def self.format(receipt)
         { "id" => receipt.id.to_s }
-    end 
+    end
 
     def self.show_total(receipt)
         { "points" => receipt.itemize }
-    end 
-end 
+    end
+end
--- spec/requests/api/v1/receipts_request_spec.rb
+++ spec/requests/api/v1/receipts_request_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Receipts API", type: 'request' do 
-    describe "POST /receipts/process" do 
-        describe "sends a receipt and returns an id" do 
-            before :each do 
+RSpec.describe "Receipts API", type: 'request' do
+    describe "POST /receipts/process" do
+        describe "sends a receipt and returns an id" do
+            before :each do
                 @receipt_params = {
                     "retailer": "Target",
                     "purchaseDate": "2022-01-01",
@@ -12,52 +12,52 @@ RSpec.describe "Receipts API", type: 'request' do
                       {
                         "shortDescription": "Mountain Dew 12PK",
                         "price": "6.49"
-                      },{
+                      }, {
                         "shortDescription": "Emils Cheese Pizza",
                         "price": "12.25"
-                      },{
+                      }, {
                         "shortDescription": "Knorr Creamy Chicken",
                         "price": "1.26"
-                      },{
+                      }, {
                         "shortDescription": "Doritos Nacho Cheese",
                         "price": "3.35"
-                      },{
+                      }, {
                         "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
                         "price": "12.00"
                       }
                     ],
                     "total": "35.35"
                   }
-                @headers = {"CONTENT_TYPE" => "application/json"}
+                @headers = { "CONTENT_TYPE" => "application/json" }
             end
-            
-            it "returns a valid response" do 
+
+            it "returns a valid response" do
                 post '/receipts/process', headers: @headers, params: JSON.generate(receipt: @receipt_params)
                 expect(response).to be_successful
-            end 
-            
-            it "creates a receipt" do 
+            end
+
+            it "creates a receipt" do
                 post '/receipts/process', headers: @headers, params: JSON.generate(receipt: @receipt_params)
                 receipt = Receipt.last
                 expect(receipt.retailer).to eq("Target")
                 expect(receipt.purchaseDate).to eq("2022-01-01")
                 expect(receipt.purchaseTime).to eq("13:01")
                 expect(receipt.total).to eq("35.35")
-            end 
-            
-            it "returns an id" do 
+            end
+
+            it "returns an id" do
                 post '/receipts/process', headers: @headers, params: JSON.generate(receipt: @receipt_params)
                 receipt = Receipt.last
                 res = JSON.parse(response.body)
                 expect(res).to be_a(Hash)
-                expect(res).to eq({ "id" => "#{receipt.id}"})
-            end 
-        end 
-    end 
-    
-    describe "GET /receipts/:id/process" do 
-        describe "sends a request and returns a 'total' numeric value" do 
-            before :each do 
+                expect(res).to eq({ "id" => "#{receipt.id}" })
+            end
+        end
+    end
+
+    describe "GET /receipts/:id/process" do
+        describe "sends a request and returns a 'total' numeric value" do
+            before :each do
                 @receipt_params = {
                     "retailer": "Target",
                     "purchaseDate": "2022-01-01",
@@ -66,16 +66,16 @@ RSpec.describe "Receipts API", type: 'request' do
                       {
                         "shortDescription": "Mountain Dew 12PK",
                         "price": "6.49"
-                      },{
+                      }, {
                         "shortDescription": "Emils Cheese Pizza",
                         "price": "12.25"
-                      },{
+                      }, {
                         "shortDescription": "Knorr Creamy Chicken",
                         "price": "1.26"
-                      },{
+                      }, {
                         "shortDescription": "Doritos Nacho Cheese",
                         "price": "3.35"
-                      },{
+                      }, {
                         "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
                         "price": "12.00"
                       }
@@ -90,29 +90,29 @@ RSpec.describe "Receipts API", type: 'request' do
                       {
                         "shortDescription": "Gatorade",
                         "price": "2.25"
-                      },{
+                      }, {
                         "shortDescription": "Gatorade",
                         "price": "2.25"
-                      },{
+                      }, {
                         "shortDescription": "Gatorade",
                         "price": "2.25"
-                      },{
+                      }, {
                         "shortDescription": "Gatorade",
                         "price": "2.25"
                       }
                     ],
                     "total": "9.00"
                   }
-                @headers = {"CONTENT_TYPE" => "application/json"}
+                @headers = { "CONTENT_TYPE" => "application/json" }
             end
 
-            it "returns a valid response" do 
+            it "returns a valid response" do
                 post '/receipts/process', headers: @headers, params: JSON.generate(receipt: @receipt_params)
                 receipt = Receipt.last
                 get "/receipts/#{receipt.id}/process"
                 expect(response).to be_successful
             end
-            
+
             it "returns a total" do
                 post '/receipts/process', headers: @headers, params: JSON.generate(receipt: @receipt_params)
                 receipt = Receipt.last
@@ -128,6 +128,6 @@ RSpec.describe "Receipts API", type: 'request' do
                 expect(res).to be_a(Hash)
                 expect(res).to eq({ "points" => 109 })
             end
-        end 
-    end 
-end 
+        end
+    end
+end
