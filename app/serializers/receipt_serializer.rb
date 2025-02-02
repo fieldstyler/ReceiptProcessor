192a5322723ee6da5327cc247e5bf086530d11a5
fieldstyler <tdfields93@gmail.com>
2025-02-02 10:15:43 -0700

    Finish format and show_total methods

--- app/serializers/receipt_serializer.rb
+++ app/serializers/receipt_serializer.rb
@@ -2,4 +2,8 @@ class ReceiptSerializer
     def self.format(receipt)
         { "id" => receipt.id.to_s }
     end 
+
+    def self.show_total(receipt)
+        { "points" => receipt.itemize }
+    end 
 end 
