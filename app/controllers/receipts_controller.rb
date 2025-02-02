b4ba3bfbbf24e0129958976c1f55afd8444481d6
fieldstyler <tdfields93@gmail.com>
2025-02-02 10:17:52 -0700

    Add and clean up receipt controller logic

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -2,9 +2,12 @@ require 'json'
 class ReceiptsController < ApplicationController
     def create 
         receipt = Receipt.create(receipt_params)
-        # i = itemize(receipt)
-        # binding.pry
         render json: ReceiptSerializer.format(receipt)
+    end 
+
+    def index 
+        receipt = Receipt.find(params[:id])
+        render json: ReceiptSerializer.show_total(receipt)
     end 
     
     private 
@@ -13,12 +16,5 @@ class ReceiptsController < ApplicationController
         params.require(:receipt).permit(:retailer, :purchaseDate, :purchaseTime, :total, :items => [:shortDescription, :price])
     end
     
-    # def itemize(receipt)
-    #     items = []
-    #     receipt.items.each do |item|
-    #         sub = item.gsub! " =>", ":"
-    #         items << JSON.parse(sub)
-    #     end 
-    #     items
-    # end 
+    
 end 
