f18f311d0d52880900b3fa8f87d6c4a5bb181406
fieldstyler <tdfields93@gmail.com>
2025-02-02 10:16:30 -0700

    Add path for get request

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   post '/receipts/process', to: "receipts#create"
+  get '/receipts/:id/process', to: "receipts#index"
 
   # Defines the root path route ("/")
   # root "posts#index"
